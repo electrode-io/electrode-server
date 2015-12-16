713ab69c1fdf63bbf06196482fe4c9a3732910b9
Joel Chen <xchen@walmartlabs.com>
2015-12-16 09:16:53 -0800

    first commit

---
+++ test/app-config-test.js
@@ -0,0 +1,41 @@
+/* eslint-env mocha */
+/* eslint-disable strict, max-statements, no-invalid-this, prefer-arrow-callback */
+"use strict";
+
+const appConfig = require("../lib/plugins/app-config.js");
+const chai = require("chai");
+
+describe("app-config-plugin", function () {
+  this.timeout(50);
+  it("should add config to req.app", function (done) {
+    let extFn;
+    const server = {
+      ext: (event, fn) => {
+        chai.assert.equal(event, "onRequest");
+        extFn = fn;
+      },
+      app: {
+        config: {
+          server: true
+        }
+      }
+    };
+    const request = {
+      app: {}
+    };
+    let continued = false;
+    const reply = {
+      "continue": () => {
+        continued = true;
+      }
+    };
+    appConfig(server, {}, () => {
+      chai.assert(extFn, "extension not registered");
+      extFn(request, reply);
+      chai.assert(continued, "continue not called");
+      chai.assert(request.app.config, "no request.app.config");
+      done();
+    });
+  });
+});
+
---
+++ test/server-test.js
@@ -0,0 +1,129 @@
+/* eslint-env mocha */
+/* eslint-disable strict, max-statements, no-invalid-this, prefer-arrow-callback */
+"use strict";
+
+const electrodeServer = require("..");
+const chai = require("chai");
+
+describe("electrode-server", function () {
+
+  it("should start up a default server twice", function (done) {
+    const x2 = () => electrodeServer({})
+      .then((server) => server.stop(done))
+      .catch(done);
+
+    electrodeServer({})
+      .then((server) => {
+        server.stop(x2);
+      })
+      .catch(done);
+  });
+
+
+  it("should start up a server twice @callbacks", function (done) {
+
+    const x2 = () => electrodeServer({}, (err, server) => server.stop(done));
+    electrodeServer({}, (err, server) => {
+      server.stop(x2);
+    });
+  });
+
+  const expectedError = () => {
+    chai.assert(false, "expected error from tested code");
+  };
+
+  it("should fail for @missing_config", function (done) {
+    electrodeServer("test/test/test1.js")
+      .then(expectedError)
+      .catch((e) => {
+        if (e.message && e.message.indexOf("Missing config file") >= 0) {
+          return done();
+        }
+        done(e);
+      });
+  });
+
+
+  it("should fail for @missing_config @callbacks", function (done) {
+    electrodeServer("test/test/test2.js", (err) => {
+      if (!err) {
+        expectedError();
+      }
+      done();
+    });
+  });
+
+
+  it("should fail for @bad_config", function (done) {
+    electrodeServer("./test/data/bad-config.js")
+      .then(expectedError)
+      .catch((e) => {
+        if (e.message && e.message.indexOf("Error in config") >= 0) {
+          return done();
+        }
+        done(e);
+      });
+  });
+
+
+  it("should try to @default_config_location", function (done) {
+    electrodeServer()
+      .then(expectedError)
+      .catch((e) => {
+        if (e.message && e.message.indexOf("config/electrode-server/server.js") >= 0) {
+          return done();
+        }
+        done(e);
+      });
+  });
+
+  it("should start up with a good @empty_config", function (done) {
+    electrodeServer("./test/data/empty-config.js")
+      .then((server) => {
+        server.stop(() => done());
+      })
+      .catch(done);
+  });
+
+
+  it("should start up with @ES6_style_config", function (done) {
+    electrodeServer("./test/data/es6-config.js")
+      .then((server) => {
+        server.stop(() => done());
+      })
+      .catch(done);
+  });
+
+
+  it("should start up with @correct_plugins_priority", function (done) {
+    electrodeServer("./test/data/server.js")
+      .then((server) => {
+        server.stop(() => done());
+      })
+      .catch(done);
+  });
+
+  it("should fail start up due to @plugin_error", function (done) {
+    electrodeServer("./test/data/plugin-err.js")
+      .then(expectedError)
+      .catch((e) => {
+        if (e._err === "plugin_failure") {
+          return done();
+        }
+        done(e);
+      });
+  });
+
+
+  it("should fail start up due to @duplicate_plugin", function (done) {
+    electrodeServer("./test/data/dup-plugin.js")
+      .then(expectedError)
+      .catch((e) => {
+        if (e.message && e.message.indexOf("error starting the Hapi.js server") >= 0) {
+          done();
+        } else {
+          done(e);
+        }
+      });
+  });
+});
