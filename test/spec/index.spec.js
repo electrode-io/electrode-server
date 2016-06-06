c50b49df1eac329db32ab1d1b6c2138ae5cfaf9c
Jason Wilson <jrwilson@gmail.com>
2016-06-06 16:01:11 -0700

    Revert "use built-in index instead of indexOf"
    This reverts commit 95d49811970be6014678ce3eb74de9436cf07332.

--- test/spec/index.spec.js
+++ test/spec/index.spec.js
@@ -290,9 +290,9 @@ describe("electrode-server", function () {
     const firedEvents = _.times(events.length, _.constant(false));
 
     const eventListener = (emitter) => {
-      _.each(events, (event, index) => {
+      _.each(events, (event) => {
         emitter.on(event, () => {
-          firedEvents[index] = true;
+          firedEvents[events.indexOf(event)] = true;
         });
       });
     };
