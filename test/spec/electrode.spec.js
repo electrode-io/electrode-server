fe13cfd9d9e45807bdd83b3244dfb9f98d453bdc
Joel Chen <xchen@walmartlabs.com>
2020-03-13 11:16:42 -0700

    update plugin registration options for hapi 17

--- test/spec/electrode.spec.js
+++ test/spec/electrode.spec.js
@@ -6,6 +6,7 @@ const assert = require("chai").assert;
 const _ = require("lodash");
 const request = require("superagent");
 const Promise = require("bluebird");
+const { asyncVerify, expectError, runFinally } = require("run-verify");
 
 const HTTP_404 = 404;
 
@@ -87,6 +88,36 @@ describe("electrode-server", function() {
     });
   });
 
+  it("should pass plugin options", () => {
+    const options = {
+      test: "foo"
+    };
+
+    let receiveOptions;
+    const register = (server, opts) => {
+      receiveOptions = opts;
+    };
+    let server;
+
+    return asyncVerify(
+      () => {
+        return electrodeServer({
+          plugins: {
+            test1: {
+              register,
+              options
+            }
+          }
+        });
+      },
+      s => {
+        server = s;
+        expect(receiveOptions).to.deep.equal(options);
+      },
+      runFinally(() => server && server.stop())
+    );
+  });
+
   it("connections without default throws error", function() {
     return electrodeServer({
       connections: {
@@ -138,7 +169,10 @@ describe("electrode-server", function() {
 
   it("should fail if plugins.requireFromPath is not string", function() {
     let error;
-    return electrodeServer({ electrode: { logLevel: "none" }, plugins: { requireFromPath: {} } })
+    return electrodeServer({
+      electrode: { logLevel: "none" },
+      plugins: { requireFromPath: {} }
+    })
       .catch(e => (error = e))
       .then(() => {
         expect(error).to.exist;
@@ -249,16 +283,16 @@ describe("electrode-server", function() {
       });
   });
 
-  it("should fail start up due to @plugin_error", function() {
-    let error;
-    return electrodeServer(require("../data/plugin-err.js"))
-      .catch(e => (error = e))
-      .then(() => {
+  it("should fail start up due to @plugin_error", () => {
+    return asyncVerify(
+      expectError(() => electrodeServer(require("../data/plugin-err.js"))),
+      error => {
         expect(error).to.exist;
         if (!_.includes(error.message, "plugin_failure")) {
           throw error;
         }
-      });
+      }
+    );
   });
 
   it("should fail start up due to @bad_plugin", function() {
@@ -273,16 +307,16 @@ describe("electrode-server", function() {
       });
   });
 
-  it("should fail start up due to @duplicate_plugin", function() {
-    let error;
-    return electrodeServer(require("../data/dup-plugin.js"))
-      .catch(e => (error = e))
-      .then(() => {
+  it("should fail start up due to @duplicate_plugin", () => {
+    return asyncVerify(
+      expectError(() => electrodeServer(require("../data/dup-plugin.js"))),
+      error => {
         expect(error).to.exist;
         if (!_.includes(error.message, "Plugin nulPlugin already registered")) {
           throw error;
         }
-      });
+      }
+    );
   });
 
   it("should fail with plugins register timeout", () => {
@@ -291,21 +325,23 @@ describe("electrode-server", function() {
         // never resolves or rejects
       });
     };
-    let error;
-    return electrodeServer({
-      plugins: {
-        test: {
-          register,
-          name: "timeout"
-        }
-      },
-      electrode: {
-        logLevel,
-        registerPluginsTimeout: 100
-      }
-    })
-      .catch(e => (error = e))
-      .then(() => {
+
+    return asyncVerify(
+      expectError(() => {
+        return electrodeServer({
+          plugins: {
+            test: {
+              register,
+              name: "timeout"
+            }
+          },
+          electrode: {
+            logLevel,
+            registerPluginsTimeout: 100
+          }
+        });
+      }),
+      error => {
         if (
           !_.includes(
             error.message,
@@ -314,7 +350,8 @@ describe("electrode-server", function() {
         ) {
           throw error;
         }
-      });
+      }
+    );
   });
 
   const testNoAbort = mode => {
